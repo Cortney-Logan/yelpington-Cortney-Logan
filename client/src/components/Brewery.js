ba645d5b347d44dd49234f778bf89cd478b83f0c
Cortney-Logan <logan.cort@gmail.com)>
2021-03-04 19:53:02 -0500

    adding json files

--- client/src/components/Brewery.js
+++ client/src/components/Brewery.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { MapContainer,TileLayer } from "react-leaflet";
 
 function Brewery(props) {
   const [breweryDetails, setBreweryDetails] = useState(null);
@@ -13,10 +14,29 @@ function Brewery(props) {
     }
   });
 
+//   let center = [43.6591, -70.2568] && [breweryDetails.lat,breweryDetails.long]
+
   return (
-    <div>
-      <h3>This is the brewery page</h3>
+    <div id="brewery-container">
       <h1>{breweryDetails && breweryDetails.name}</h1>
+      <div id="location-and-hours-container">
+        <div id="map-container">
+          <MapContainer
+            center={[43.6591, -70.2568]}
+            zoom={12}
+            style={{ height: "600px", width: "600px" }}
+          >
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            />
+          </MapContainer>
+        </div>
+        <div id="hours-container">
+          <ul></ul>
+          <p>{breweryDetails && breweryDetails.address}</p>
+        </div>
+      </div>
     </div>
   );
 }
